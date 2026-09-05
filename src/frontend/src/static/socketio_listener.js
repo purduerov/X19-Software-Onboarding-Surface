37064c06cddd85fd49af4eebdfe3ac50fe01624b
KiraYang3 <kirayang02@gmail.com>
2026-09-05 15:35:14 -0400

    removed depth subscriber node example (node file, yaml, protobuf message typing, and socketio js file)

--- src/frontend/src/static/socketio_listener.js
+++ src/frontend/src/static/socketio_listener.js
@@ -38,16 +38,6 @@ socket.on('surface_imu', function(msg) {
     document.getElementById("angular-command").innerHTML = "Pitch: " + msg.twist.angular.x.toFixed(3) + "<br>Roll: " + msg.twist.angular.y.toFixed(3) + "<br>Yaw: " + msg.twist.angular.z.toFixed(3);
 });
 
-socket.on('depth', function(msg){
-    // Log the message to the browser console
-    // console.log("New message received:", msg);
-    // Parse the message into json
-    msg = JSON.parse(msg);
-
-    // Insert the following x: 0<br>y: 0<br>z: 0 into the HTLM div with id linear-command
-    document.getElementById("depth-data").innerHTML = `${msg.data.toFixed(2)} m`;
-});
-
 socket.on('hat_temp', function(msg){
     // Log the message to the browser console
     // console.log("New message received:", msg);
